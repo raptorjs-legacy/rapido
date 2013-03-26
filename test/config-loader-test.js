f61321259d8c8cd88b1a82e952cec93c349cd56c
Patrick Steele-Idem <psteeleidem@ebay.com>
2013-03-26 00:15:56 -0600

    Updated test cases

--- test/config-loader-test.js
+++ test/config-loader-test.js
@@ -16,15 +16,15 @@ describe('config-loader', function(){
 
             var stacks = rapido.commands.getStacks();
             stacks.should.have.length(3);
-            stacks[0].name.should.equal('all');
-            stacks[0].description.should.equal('Commands that apply to all stacks');
+            stacks[0].name.should.equal('default');
+            stacks[0].description.should.be.a('string');
             stacks[0].getCommands().should.have.length(2);
-            stacks[0].getCommands()[0].name.should.equal("all command1");
-            stacks[0].getCommands()[0].description.should.equal("Description for all command1");
-            stacks[0].getCommands()[0].file.getAbsolutePath().should.equal(path.join(__dirname, 'resources/all-command1.js'));
-            stacks[0].getCommands()[1].name.should.equal("all command2");
-            stacks[0].getCommands()[1].description.should.equal("Description for all command2");
-            stacks[0].getCommands()[1].file.getAbsolutePath().should.equal(path.join(__dirname, 'resources/all-command2.js'));
+            stacks[0].getCommands()[0].name.should.equal("command1");
+            stacks[0].getCommands()[0].description.should.equal("Description for command1");
+            stacks[0].getCommands()[0].file.getAbsolutePath().should.equal(path.join(__dirname, 'resources/command1.js'));
+            stacks[0].getCommands()[1].name.should.equal("command2");
+            stacks[0].getCommands()[1].description.should.equal("Description for command2");
+            stacks[0].getCommands()[1].file.getAbsolutePath().should.equal(path.join(__dirname, 'resources/command2.js'));
 
             stacks[1].name.should.equal('stack1');
             stacks[1].description.should.equal('Description for stack1');
